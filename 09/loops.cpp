cbaee5cec0cfc4f83fa4b5bdd75765d491cf244d
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-01-19 21:05:35 -0500

    loops

---
+++ 09/loops.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+ 
+ int main(){
+     //for loop
+    // for(int i=1;i<=100;i++){
+    //     cout<<i<<endl;
+    // }     
+
+    // infinite for loop
+    // for(int i=1;34<=100;i++){
+    //     cout<<i<<endl;
+    // }   
+
+
+     //while loop
+    //  int i = 1;
+    // while (i<=40)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    
+    //infinite while 
+    // int i = 1;
+    // while (true)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    
+
+    //  //do while loop
+    //   int i = 1;
+    //  do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // while (i<=40);
+
+     //table using do while loop
+      int i = 1, num = 5;
+     do{
+        cout<<num*i<<endl;
+        i++;
+    }
+    while (i<=10);
+
+     return 0;
+ }
