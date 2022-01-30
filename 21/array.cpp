8de8591ce10afbce6aa7b9cc100928b26009a0be
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-01-29 19:39:18 -0500

    allocating memory using array

---
+++ 21/array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class shop
+{
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) { counter = 0; }
+    void displayPrice(void);
+    void setPrice(void);
+};
+
+void shop ::setPrice(void)
+{
+    cout << "Enter Id of your Item no: "<<counter+1 << endl;
+    cin >> itemId[counter];
+    cout << "Enter price of yout Item: " << endl;
+    cin >> itemPrice[counter];
+    counter++;
+}
+
+void shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout<<"The price of item with Id"<<itemId[i]<<" is "<<itemPrice[i]<<endl;
+    }
+    
+}
+
+int main()
+{
+    shop dukan;
+    dukan.initCounter();
+    dukan.setPrice();
+    dukan.setPrice();
+    dukan.setPrice();
+    dukan.displayPrice();
+    return 0;
+}
