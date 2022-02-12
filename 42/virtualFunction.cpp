9b27fc6adec12915d1a79a8d4b08efead71fdb43
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-02-12 10:48:49 -0500

    virtual function

---
+++ 42/virtualFunction.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class BaseClass
+{
+    public:
+    int var_base = 1;
+    virtual void display(void){
+        cout<<"Displaying Base class variable var 1: "<<var_base<<endl;
+    }
+};
+
+class DerivedClass : public BaseClass
+{
+    public:
+    int var_derived = 2;
+    void display(void){
+        cout<<"Displaying Base class variable var 1: "<<var_base<<endl;
+        cout<<"Displaying Derived class variable var 1: "<<var_derived<<endl;
+    }
+};
+
+int main(){
+    BaseClass *base_class_pointer;
+    BaseClass objBase;
+    DerivedClass objDerived;
+    base_class_pointer  = &objDerived;
+    base_class_pointer->display();
+    return 0;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
